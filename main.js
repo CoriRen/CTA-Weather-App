be7ae24484a8f39ee84354e192e63b7956ddc15f
Corinne GoodSmith <corinnegoodsmith@gmail.com>
2024-09-13 18:45:47 -0500

    added weather grid functionality

--- main.js
+++ main.js
@@ -19,19 +19,80 @@ class WeatherFetcher {
       .then(res => res.json())
       .then(data => {
         console.log(data)
+
+        //Inserts current temp and short weather forecast
         const shortForecast = data.properties.periods[0].shortForecast.toLowerCase();
-        document.querySelector('.currentTemp').innerText = data.properties.periods[0].temperature;
+        const currentTemp = data.properties.periods[0].temperature;
+
+        document.querySelector('.currentTemp').innerText = currentTemp
         document.querySelector('.shortForecast').innerText = shortForecast
-        if (shortForecast === 'sunny' || shortForecast === 'mostly sunny'){
-          document.querySelector('.weatherIcon').style.backgroundImage = "url('assets/sunny.png')"
-        }else if (String(shortForecast).includes('showers') ){
-          document.querySelector('.weatherIcon').style.backgroundImage = "url('assets/rain-clouds.png')"
-        }else if (String(shortForecast).includes('snow') ){
-          document.querySelector('.weatherIcon').style.backgroundImage = "url('assets/snowfall.gif')"
-        }else {
-          document.querySelector('.weatherIcon').style.backgroundImage = "url('assets/Clouds.png')"
+
+        //Inserts forecast data into the hourly forecast grid 
+        const futureTemp1 = data.properties.periods[2].temperature;
+        const futureTemp2 = data.properties.periods[4].temperature;
+        const futureTemp3 = data.properties.periods[6].temperature;
+        const futureTemp4 = data.properties.periods[8].temperature;
+        const futureTemp5 = data.properties.periods[10].temperature;
+
+        document.querySelector('#futureTemp1').innerText = futureTemp1;
+        document.querySelector('#futureTemp2').innerText = futureTemp2;
+        document.querySelector('#futureTemp3').innerText = futureTemp3;
+        document.querySelector('#futureTemp4').innerText = futureTemp4;
+        document.querySelector('#futureTemp5').innerText = futureTemp5;
+
+        //Inserts forecast start time into the forecast grid 
+        const futureTime1 = data.properties.periods[2].startTime;
+        const futureTime2 = data.properties.periods[4].startTime;
+        const futureTime3 = data.properties.periods[6].startTime;
+        const futureTime4 = data.properties.periods[8].startTime;
+        const futureTime5 = data.properties.periods[10].startTime;
+
+        document.querySelector('#futureTime1').innerText = new Date(futureTime1).toLocaleTimeString(navigator.language, { timeStyle: 'short'});
+        document.querySelector('#futureTime2').innerText = new Date(futureTime2).toLocaleTimeString(navigator.language, { timeStyle: 'short'});
+        document.querySelector('#futureTime3').innerText = new Date(futureTime3).toLocaleTimeString(navigator.language, { timeStyle: 'short'});
+        document.querySelector('#futureTime4').innerText = new Date(futureTime4).toLocaleTimeString(navigator.language, { timeStyle: 'short'});
+        document.querySelector('#futureTime5').innerText = new Date(futureTime5).toLocaleTimeString(navigator.language, { timeStyle: 'short'});
+
+        //inserts chance of rain into forecast grid
+        const futureRain1 = data.properties.periods[2].probabilityOfPrecipitation.value ?? 0;
+        const futureRain2 = data.properties.periods[4].probabilityOfPrecipitation.value ?? 0;
+        const futureRain3 = data.properties.periods[6].probabilityOfPrecipitation.value ?? 0;
+        const futureRain4 = data.properties.periods[8].probabilityOfPrecipitation.value ?? 0;
+        const futureRain5 = data.properties.periods[10].probabilityOfPrecipitation.value ?? 0;
+
+        document.querySelector('#futureRain1').innerText = futureRain1;
+        document.querySelector('#futureRain2').innerText = futureRain2;
+        document.querySelector('#futureRain3').innerText = futureRain3;
+        document.querySelector('#futureRain4').innerText = futureRain4;
+        document.querySelector('#futureRain5').innerText = futureRain5;
+    
+
+        //Selects weather icon based on the shortForecast variable
+        const weatherIcons = {
+          sunny: "url('assets/sunny.png')",
+          mostlySunny: "url('assets/partly-sunny.png')",
+          showers: "url('assets/rain-clouds.png')",
+          snow: "url('assets/snowfall.gif')",
+          default: "url('assets/Clouds.png')"
         }
-      })
+         
+        let weatherIcon = weatherIcons['default'];
+
+        if (shortForecast.includes('mostly sunny')){
+         weatherIcon = weatherIcons['mostlySunny']
+
+        } else if (shortForecast.includes('showers')){
+          weatherIcon = weatherIcons['showers']
+
+        } else if (shortForecast.includes('snow')){
+          weatherIcon = weatherIcons['snow']
+
+        }else if (shortForecast.includes('sunny')){
+          weatherIcon = weatherIcons['Sunny']
+        }
+
+          document.querySelector('.weatherIcon').style.backgroundImage = weatherIcon
+        })
       .catch(err => {
         console.log(`error ${err}`);
       });
@@ -42,10 +103,20 @@ class WeatherFetcher {
       .then(res => res.json())
       .then(data => {
         console.log(data)
-        document.querySelector('.detailedForecast').innerText = data.properties.periods[0].detailedForecast;
+
+        //document.querySelector('.detailedForecast').innerText = data.properties.periods[0].detailedForecast;
+
         const chanceOfRain = data.properties.periods[0].probabilityOfPrecipitation.value ?? 0;
+        
+        const highTemp = data.properties.periods[0].temperature;
+
         document.querySelector('#chanceOfRain').innerText = chanceOfRain;
-        document.querySelector('#highTemp').innerText=data.properties.periods[0].temperature;
+
+        document.querySelector('#highTemp').innerText= highTemp;
+
+        document.querySelector('#period1').innerText= highTemp;
+
+
       })
       .catch(err => {
         console.log(`error ${err}`);
